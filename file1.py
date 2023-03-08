33a18f938a86276d68af9c5500c92e96deee8c17
Jain <priyank.jain@sms-group.com>
2023-03-08 20:09:34 +0530

    file1 and file2 are deleted

--- file1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from IPython.display import display
-
-print('this is file1')
-nbafile = pd.read_csv('data/nba.csv')
-
-display(nbafile)
-nbafile.to_csv('data/nba2.csv', index_label=False)
--- file2.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-print('this file is file2')
-pd.read_csv('data/nba2.csv')
